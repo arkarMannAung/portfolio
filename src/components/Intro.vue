5f1d1d7b2b09cbd4a3b3caeb163d5501e841bebe
arkarMannAung <mr.arkarmannaung@gmail.com>
2023-01-14 14:29:22 +0630

    change

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,14 +1,13 @@
 <template>
-  <div id="Intro" class="w-full h-184 sm:h-208 lg:h-184 bg-silver-gray flex flex-col justify-end">
+  <div id="Intro" class="w-full h-208 sm:h-full lg:h-184 bg-silver-gray flex flex-col justify-end">
     <div class="flex flex-col lg:flex-row justify-center h-full mt-14 items-center">
       <div class="text-gray-300 lg:mr-6 mb-3 lg:mb-0 text-sm mt-1">
         Create With
       </div>
       <svg viewBox="0 0 248 31" class="text-gray-300 dark:text-white w-auto h-4"><path fill-rule="evenodd" clip-rule="evenodd" d="M25.517 0C18.712 0 14.46 3.382 12.758 10.146c2.552-3.382 5.529-4.65 8.931-3.805 1.941.482 3.329 1.882 4.864 3.432 2.502 2.524 5.398 5.445 11.722 5.445 6.804 0 11.057-3.382 12.758-10.145-2.551 3.382-5.528 4.65-8.93 3.804-1.942-.482-3.33-1.882-4.865-3.431C34.736 2.92 31.841 0 25.517 0zM12.758 15.218C5.954 15.218 1.701 18.6 0 25.364c2.552-3.382 5.529-4.65 8.93-3.805 1.942.482 3.33 1.882 4.865 3.432 2.502 2.524 5.397 5.445 11.722 5.445 6.804 0 11.057-3.381 12.758-10.145-2.552 3.382-5.529 4.65-8.931 3.805-1.941-.483-3.329-1.883-4.864-3.432-2.502-2.524-5.398-5.446-11.722-5.446z" fill="#38bdf8"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M76.546 12.825h-4.453v8.567c0 2.285 1.508 2.249 4.453 2.106v3.463c-5.962.714-8.332-.928-8.332-5.569v-8.567H64.91V9.112h3.304V4.318l3.879-1.143v5.937h4.453v3.713zM93.52 9.112h3.878v17.849h-3.878v-2.57c-1.365 1.891-3.484 3.034-6.285 3.034-4.884 0-8.942-4.105-8.942-9.389 0-5.318 4.058-9.388 8.942-9.388 2.801 0 4.92 1.142 6.285 2.999V9.112zm-5.674 14.636c3.232 0 5.674-2.392 5.674-5.712s-2.442-5.711-5.674-5.711-5.674 2.392-5.674 5.711c0 3.32 2.442 5.712 5.674 5.712zm16.016-17.313c-1.364 0-2.477-1.142-2.477-2.463a2.475 2.475 0 012.477-2.463 2.475 2.475 0 012.478 2.463c0 1.32-1.113 2.463-2.478 2.463zm-1.939 20.526V9.112h3.879v17.849h-3.879zm8.368 0V.9h3.878v26.06h-3.878zm29.053-17.849h4.094l-5.638 17.849h-3.807l-3.735-12.03-3.771 12.03h-3.806l-5.639-17.849h4.094l3.484 12.315 3.771-12.315h3.699l3.734 12.315 3.52-12.315zm8.906-2.677c-1.365 0-2.478-1.142-2.478-2.463a2.475 2.475 0 012.478-2.463 2.475 2.475 0 012.478 2.463c0 1.32-1.113 2.463-2.478 2.463zm-1.939 20.526V9.112h3.878v17.849h-3.878zm17.812-18.313c4.022 0 6.895 2.713 6.895 7.354V26.96h-3.878V16.394c0-2.713-1.58-4.14-4.022-4.14-2.55 0-4.561 1.499-4.561 5.14v9.567h-3.879V9.112h3.879v2.285c1.185-1.856 3.124-2.749 5.566-2.749zm25.282-6.675h3.879V26.96h-3.879v-2.57c-1.364 1.892-3.483 3.034-6.284 3.034-4.884 0-8.942-4.105-8.942-9.389 0-5.318 4.058-9.388 8.942-9.388 2.801 0 4.92 1.142 6.284 2.999V1.973zm-5.674 21.775c3.232 0 5.674-2.392 5.674-5.712s-2.442-5.711-5.674-5.711-5.674 2.392-5.674 5.711c0 3.32 2.442 5.712 5.674 5.712zm22.553 3.677c-5.423 0-9.481-4.105-9.481-9.389 0-5.318 4.058-9.388 9.481-9.388 3.519 0 6.572 1.82 8.008 4.605l-3.34 1.928c-.79-1.678-2.549-2.749-4.704-2.749-3.16 0-5.566 2.392-5.566 5.604 0 3.213 2.406 5.605 5.566 5.605 2.155 0 3.914-1.107 4.776-2.749l3.34 1.892c-1.508 2.82-4.561 4.64-8.08 4.64zm14.472-13.387c0 3.249 9.661 1.285 9.661 7.89 0 3.57-3.125 5.497-7.003 5.497-3.591 0-6.177-1.607-7.326-4.177l3.34-1.927c.574 1.606 2.011 2.57 3.986 2.57 1.724 0 3.052-.571 3.052-2 0-3.176-9.66-1.391-9.66-7.781 0-3.356 2.909-5.462 6.572-5.462 2.945 0 5.387 1.357 6.644 3.713l-3.268 1.82c-.647-1.392-1.904-2.035-3.376-2.035-1.401 0-2.622.607-2.622 1.892zm16.556 0c0 3.249 9.66 1.285 9.66 7.89 0 3.57-3.124 5.497-7.003 5.497-3.591 0-6.176-1.607-7.326-4.177l3.34-1.927c.575 1.606 2.011 2.57 3.986 2.57 1.724 0 3.053-.571 3.053-2 0-3.176-9.66-1.391-9.66-7.781 0-3.356 2.908-5.462 6.572-5.462 2.944 0 5.386 1.357 6.643 3.713l-3.268 1.82c-.646-1.392-1.903-2.035-3.375-2.035-1.401 0-2.622.607-2.622 1.892z" fill="currentColor"></path></svg>
-
     </div>
     <div class="lg:container lg:mx-auto flex flex-col lg:flex-row lg:items-end">
-      <div class="flex flex-1 justify-center">
+      <div class="mb-20 flex flex-1 justify-center">
         <div class="w-72 h-72 sm:w-104 sm:h-104 lg:w-112 lg:h-112 rounded-full bg-circle shadow-2xl">
           <div class="relative">
             <div class="absolute left-0 top-56 sm:top-64 lg:left-2 lg:top-60 w-16 h-16 sm:w-20 sm:h-20 rounded-full shadow-2xl bg-silver-gray">
@@ -18,7 +17,7 @@
               <img class="h-16 sm:h-20 rounded-full p-3 shadow-xl" src="@/assets/images/javascript.jpg" alt="">
             </div>
             <div class="absolute right-16 sm:right-36 lg:left-80 lg:top-24 w-16 h-16 sm:w-20 sm:h-20 rounded-full shadow-2xl bg-silver-gray">
-              <img class="h-16 sm:h-20 rounded-sm p-3 shadow-xl" src="@/assets/images/react.png" alt="">
+              <img class="animate-spin h-16 sm:h-20 rounded-sm p-3" src="@/assets/images/react.png" alt="">
             </div>
             <div class="absolute left-0 top-6 lg:left-0 lg:top-4 2-16 h-16 sm:w-20 sm:h-20 rounded-full shadow-2xl bg-silver-gray">
               <img class="h-16 sm:h-20 rounded-sm p-3 shadow-xl" src="@/assets/images/laravel.png" alt="">
@@ -27,14 +26,15 @@
               <img class="h-16 sm:h-20 rounded-full p-3 shadow-xl" src="@/assets/images/vue.png" alt="">
             </div>
             <div class="absolute left-12 sm:left-16 top-24 sm:top-36 lg:left-20 lg:top-32 2-16 h-16 sm:w-20 sm:h-20 rounded-full shadow-2xl bg-silver-gray">
-              <img class="h-16 sm:h-20 rounded-full p-3 shadow-xl" src="@/assets/images/php.jpg" alt="">
+              <img class="animate-bounce h-16 sm:h-20 rounded-full p-3 shadow-xl" src="@/assets/images/php.jpg" alt="">
             </div>
           </div>
         </div>
-        <div class="absolute">
-            <img class="h-72 sm:h-104 lg:h-112 pt-24" src="@/assets/images/my_photo.png" alt="">
+        <div class="absolute w-72 h-72 sm:w-104 sm:h-104 lg:w-112 lg:h-112 rounded-full overflow-hidden">
+            <div class="flex flex-col items-center justify-end h-full">
+              <img class="w-2/3" src="@/assets/images/my_photo.png" alt="">
+            </div>
         </div>
-        
       </div>
       <div class="flex flex-1 h-full justify-center lg:items-center pt-6">
         <div class="flex flex-col items-center lg:items-start lg:pl-10 lg:text-left select-none">
