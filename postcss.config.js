95d524c0d1cf73e1a54f3364ad4e87a9eca30ce5
Arkar Mann Aung <74583828+arkarMannAung@users.noreply.github.com>
2023-02-12 05:50:26 +0630

    cv form

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,44 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
   content: [],
   theme: {
+    extend: {
+      colors: {
+        'silver-gray': '#2d2e32',
+        'dark-gray': '#25262a',
+        'circle': '#29282a',
+        'text-gray': '#9898a0',
+      },
+      width: {
+        '104': '26rem',
+        '112': '28rem',
+        '120': '30rem',
+        '128': '32rem',
+        '136': '34rem',
+        '144': '36rem',
+        '152': '38rem',
+        '160': '40rem',
+        '184': '46rem',
+        '208': '52rem',
+      },
+      height: {
+        '104': '26rem',
+        '112': '28rem',
+        '120': '30rem',
+        '128': '32rem',
+        '136': '34rem',
+        '144': '36rem',
+        '152': '38rem',
+        '160': '40rem',
+        '184': '46rem',
+        '208': '52rem',
+      },
+      transitionDuration: {
+        '1500': '1500ms',
+        '2000': '2000ms',
+        '2500': '2500ms',
+        '3000': '3000ms',
+      }
+    },
   },
   plugins: {
     tailwindcss: {},
